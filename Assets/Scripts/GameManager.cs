be496698742aed5eff3225331cd6d9cc550ccfe9
xpinar <107260019+xpinar@users.noreply.github.com>
2025-12-10 12:18:29 +0100

    singleton GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,17 +6,27 @@ public class GameManager : MonoBehaviour
     public BoardManager BoardManager;
     public PlayerController PlayerController;
 
-    //public TurnManager TurnManager { get; private set; }
+    public TurnManager TurnManager { get; private set; }
 
     public static GameManager Instance { get; private set; }
 
-    void Awake()
+    private void Awake()
     {
-        // TODO : initialiser le singleton
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
     }
 
     void Start()
     {
-        // TODO : “instancier” et créer les objets
+        //TurnManager = new TurnManager();
+
+        //BoardManager.Init();
+        //PlayerController.Spawn(BoardManager, new Vector2Int(1, 1));
     }
 }
